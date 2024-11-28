d5ab8a823c7f1af50c2975f205fff523ace962a5
UdlaAriel <ariel.anchapaxi@udla.edu.ec>
2024-11-28 18:40:10 -0500

    Cambios en el lenguaje de la aplicación.

--- AnchapaxiApuntesApp/Models/About.cs
+++ AnchapaxiApuntesApp/Models/About.cs
@@ -8,9 +8,9 @@ namespace AnchapaxiApuntesApp.Models
 {
     internal class About
     {
-        public string Title => AppInfo.Name;
+        public string Title => "Bloc de notas";
         public string Version => AppInfo.VersionString;
-        public string MoreInfoUrl => "https://aka.ms/maui";
-        public string Message => "This app is written in XAML and C# with .NET MAUI.";
+        public string MoreInfoUrl => "https://www.youtube.com/watch?v=UiNjXsuYFXw";
+        public string Message => "Hola mi nombre es Ariel Anchapaxi, me gustan mucho los memes ecuatorianos y no me gusta vivir sin luz.";
     }
 }
