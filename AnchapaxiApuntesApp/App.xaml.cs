469f472928671d7a7bdf09b63c90452ded1771f9
ARIEL <ARIEL@DESKTOP-BEFKEM8>
2024-11-28 18:06:03 -0500

    Agregar archivos de proyecto.

---
+++ AnchapaxiApuntesApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace AnchapaxiApuntesApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ AnchapaxiApuntesApp/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace AnchapaxiApuntesApp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
